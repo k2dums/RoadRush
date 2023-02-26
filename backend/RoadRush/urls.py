8eca5ba7c96307e3d54a531ab4f63a8b61ee5097
k2dums <norbu.paddu@gmail.com>
2023-02-26 21:24:50 +0530

    changed the swagger title to RoadRush

--- backend/RoadRush/urls.py
+++ backend/RoadRush/urls.py
@@ -21,7 +21,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title=" RideShare Api",
+      title=" RoadRush Api",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.ourapp.com/policies/terms/",
