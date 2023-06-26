ea5d76d1c292105b66bedb3b4652c0c913c95523
k2dums <norbu.paddu@gmail.com>
2023-06-26 22:35:17 +0530

    added the behaviour to createTrip and endTrip

--- backend/trips/manager.py
+++ backend/trips/manager.py
@@ -1,17 +1,20 @@
 from driver.manager import DriverManager
 from rider.manager import RiderManager
-from stratergies.cabMatchingStratergy import CabMatchingStratergy
-from stratergies.pricingStratergy import PricingStratergy
-
+# from stratergies.cabMatchingStratergy import CabMatchingStratergy
+# from stratergies.pricingStratergy import PricingStratergy
+from .models import Trips
 class TripsManager:
     @classmethod
-    def createTrip(cls,username,carId,location,destination):
+    def createTrip(cls,username,carId,origin,destination):
         """Creates trip based on the the username and carId"""
-        pass
+        driver=DriverManager.getDriver(carId)
+        rider=RiderManager.getRider(username)
+        trip=Trips(rider=rider,driver=driver,origin=origin,destination=destination,status=Trips.Trip_Status.BOOKED)
+        DriverManager.updateDriverOccupiedStatus_driverObj(driver,True)
+        trip.save()
 
     def endTrip(cls,carId):
         """Ends the trip of the car"""
-        car=DriverManager.getDriver(carId)
-        DriverManager.updateDriverAvailability(carId,False)
+        DriverManager.updateDriverOccupiedStatus(carId,False)
 
 
