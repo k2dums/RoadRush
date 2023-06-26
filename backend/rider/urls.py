99a34a74a74f8b9152f30d6982a2757113cdc52e
k2dums <norbu.paddu@gmail.com>
2023-06-26 22:30:18 +0530

    changed /test url '', added /all_riders and /logout path

--- backend/rider/urls.py
+++ backend/rider/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+
 urlpatterns=[
-    path('test/',views.test,name='riderTest'),
+    path('',views.test,name='riderTest'),
   
 
     #-------------------API route------------------------#
     path('login/',views.loginView,name='rider_login'),
+    path('logout/',views.logOutView,name='rider_logout'),
     path('register/',views.registerView,name='rider_register'),
     path('<str:username>/',views.rider_details,name='rider_details'),
     path('book/<str:username>/',views.book,name='rider_book'),
     path('<str:username>/test_loc/',views.rider_test_loc,name='rider_test_loc'),
+    path('all_riders',views.all_riders)
 ]
