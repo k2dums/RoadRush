448f635bd87a456ea508049f20b90266ed1ad7d2
k2dums <norbu.paddu@gmail.com>
2023-06-26 22:36:23 +0530

    added new trip status, and orgin,destination to the model field. Trip Status is now a Choices

--- backend/trips/models.py
+++ backend/trips/models.py
@@ -1,18 +1,22 @@
-from django.db import models
+from django.contrib.gis.db import models
 from driver.models import Driver
 from rider.models import Rider
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 class Trips(models.Model):
-    class Trip_Status:
-        CANCELLED='cancelled'
-        COMPLETED='completed'
-        ONGOING='ongoing'
+    class Trip_Status(models.TextChoices):
+        BOOKED='BOOKED'
+        CANCELLED='CANCELLED'
+        COMPLETED='COMPLETED'
+        ONGOING='ONGOING'
+        NA='NA'
     rider=models.ForeignKey(Rider,on_delete=models.CASCADE,related_name="rider_trips")
     driver=models.ForeignKey(Driver,on_delete=models.CASCADE,related_name="driver_trips")
     time_start=models.DateField(auto_created=True)
     time_completed=models.DateField(default=None)
     duration=models.TimeField(default=None)
-    status=Trip_Status.ONGOING
+    status=models.CharField(_('Status'),max_length=20,choices=Trip_Status.choices,default=Trip_Status.NA)
+    origin=models.PointField()
+    destination=models.PointField()
 
