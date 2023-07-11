226e09731fe60a881ac0f623f3cc9de6a04ffa79
k2dums <norbu.paddu@gmail.com>
2023-07-11 21:12:14 +0530

    default path for rider now call views.index()
    new test path is rider/test. Removed the username arg from rider/username/test_loc -> rider/test_loc

--- backend/rider/urls.py
+++ backend/rider/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,RedirectView
 
 urlpatterns=[
-    path('',views.test,name='riderTest'),
-  
-
     #-------------------API route------------------------#
+    path('',views.index,name='index'),
+    path('test/',views.rider_test,name='rider_test'),
+    path('test_loc/',views.rider_test_loc,name='rider_test_loc'),
     path('login/',views.loginView,name='rider_login'),
     path('logout/',views.logOutView,name='rider_logout'),
     path('register/',views.registerView,name='rider_register'),
     path('<str:username>/',views.rider_details,name='rider_details'),
     path('book/<str:username>/',views.book,name='rider_book'),
-    path('<str:username>/test_loc/',views.rider_test_loc,name='rider_test_loc'),
-    path('all_riders',views.all_riders)
+    path('all_riders',views.all_riders),
 ]
