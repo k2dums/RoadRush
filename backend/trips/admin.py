dca1a4a7a64e4266df8765c3ac65ff9617efee2a
k2dums <norbu.paddu@gmail.com>
2023-07-12 13:04:31 +0530

    added trips to admin with OSMGeoAdmin

--- backend/trips/admin.py
+++ backend/trips/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Trips
 # Register your models here.
+from django.contrib.gis.admin import OSMGeoAdmin
+@admin.register(Trips)
+class TripsAdmin(OSMGeoAdmin):
+    list_display = ("id","rider",'driver','status','origin','destination')
