c1d716c2a037be7bb20f8e1f1410d7cfdd3c4402
k2dums <norbu.paddu@gmail.com>
2023-09-09 16:01:26 +0530

    changed the routing path variable from driver_id to drive_username

--- backend/driver/routing.py
+++ backend/driver/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
 from . import consumers
 websocket_urlpatterns=[
-    re_path(r"ws/driver/(?P<driver_id>\w+)/$",consumers.DriverConsumer.as_asgi())
+    re_path(r"ws/driver/(?P<driver_username>\w+)/$",consumers.DriverConsumer.as_asgi())
 ]
