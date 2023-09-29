d4f60637ca9b2535ffc75b4e707c7660e7994dcf
k2dums <norbu.paddu@gmail.com>
2023-09-29 01:12:49 +0100

    create RequestType for various states of the trips

---
+++ backend/common/request.py
@@ -0,0 +1,8 @@
+class RequestType:
+    TripAccept="TRIP_ACCEPT"
+    TripEnd='TRIP_END'
+    TripCancel='TRIP_CANCEL'
+    TripSearch='TRIP_SEARCH'
+    TripTest='TRIP_TEST'
+    TripBid='BID_REQUEST'
+    TripBidAccept='BID_ACCEPT'
