db95e5ed35ca497d95007db226f0f7aafcfd3e85
k2dums <norbu.paddu@gmail.com>
2023-09-29 01:18:53 +0100

    add new functions to the tripController
    endTrip(), cancelTrip(), endTrip_byObjects() , cancelTrip_byObjects(), check_TripIntegrity()

--- backend/controllers/tripsController.py
+++ backend/controllers/tripsController.py
@@ -14,5 +14,17 @@ class TripsController:
     # def createTrip_riderUsername(cls,username,carId,origin,destination):
     #     return TripsManager.createTrip_riderUsername(username,carId,origin,destination)
     @classmethod
-    def endTrip(cls,carId,username,action):
-        return TripsManager.endTrip(carId,username,action)
+    def endTrip(cls,carId,username):
+        return TripsManager.endTrip(carId,username)
+    @classmethod
+    def cancelTrip(cls,carId,username):
+        return TripsManager.cancelTrip(carId,username,)
+    @classmethod
+    def endTrip_byObjects(cls,riderObj,driverObj,tripObj):
+        return TripsManager.endTrip_byObjects(riderObj,driverObj,tripObj)
+    @classmethod
+    def cancelTrip_byObjects(cls,riderObj,driverObj,tripObj):
+        return TripsManager.cancelTrip_byObjects(riderObj,driverObj,tripObj)
+    @classmethod
+    def check_TripIntegrity(cls,riderObj,driverObj):
+        return TripsManager.check_TripIntegrity(riderObj,driverObj)
